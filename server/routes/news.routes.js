54c633c5c7d55d9082ea2c3d9f6dd5333d94fa25
Kyle Deato <kyledeato@gmail.com>
2022-06-07 13:55:22 -0700

    no server

--- server/routes/news.routes.js
+++
@@ -1,8 +0,0 @@
-const NewsController = require("../controllers/news.controller")
-
-module.exports = function(app){
-    app.get("/api/news", NewsController.getAllNews)
-    app.get("/api/news/:id", NewsController.getOneNews)
-    app.delete("/api/news/:id", NewsController.deleteNews)
-}
-
